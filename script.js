e763f85c44ca5cca77f7ab8d9b5baeb424d08aec
Your Name <you@example.com>
2023-09-12 13:08:31 +0300

    i dont understand how it works

---
+++ script.js
@@ -0,0 +1,7 @@
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
---
+++ suggestions.js
@@ -0,0 +1,209 @@
+let suggestions = [
+
+Keskusta 
+
+Kalevantie
+
+Pogostantie
+
+Möhköllä
+
+  
+
+1. Megrin matkailu
+
+2 
+
+3 Vaara-Karjalan Osuuspankki
+
+4 Kotimäen Pitotupa
+
+5. ilomantsin apteekki 
+
+6 
+
+7 Puoti & Sumppila
+
+8 
+
+9 taukoherkut 
+
+10 Ravintola Parppeinpirtti
+11 Ilonakoti
+
+12 Katariina luomutila
+
+13 B&B kuuksenkaari
+
+14 Hermannin Viinitila
+
+15 Hermannin Viinitorni
+
+16 Havel
+
+17 Karjalan Rakentajat
+
+18 Anssilan Maatila
+19 Petkeljärvi Center
+
+20 Puustilan maisematila
+
+21 Savottakahvila Möhkön Manta 
+
+22 Raskaan kaluston palvelut 
+
+23 Pogostan Hovi
+
+24 Pogostan Kaluste
+
+25 Ark-konttori l&k
+
+26 Ilomantsin 4H-yhdistys
+
+27 Ilomantsin leipomo
+
+28 elämysmantsi 
+
+29 koitadesing 
+
+30 Kesport Ilomantsi
+
+31 K-Market Ilomantsi
+
+32 Tili-Talo
+
+33 Putkiasennus Räty
+
+34 Pogostan Rauta 
+
+35 
+
+36 Metsä- ja maansiirto Kastinen Oy 
+
+37 Ilomantsin Kivituote
+
+38 Ilonet
+
+39 Kone- ja Autotarvike Tanskanen Oy 
+
+40 Kuuksenvaaran Konepalvelu
+
+41 Mantsin Kehys
+42 AD Autokorjaamo S. Martiskainen Oy
+
+43 Mantan Majatalo  
+
+43 Mantan Majatalo 
+
+44 Möhkön Ruukki
+
+45 Fysio Voimala 
+46 jalkojen hoito elämysmantsi 
+
+47 Mottimaja
+48 
+
+49 STC Tuotanto
+
+50 Ilomantsin kunta
+
+51 Liikuntahalli
+52 Uimahalli
+
+53 Pogostan Tietoverkkopalvelut 
+
+54 Kansallistalo 
+55 Arskan Kone Oy 
+
+56 Kaivuu ja Kuljetus Kari Kuivalainen Oy 
+
+57 Runonlaulajan pirtti 
+
+58 Katri Vala Kulttuurikeskus 
+59 Koveron Majatalo
+
+60 Salakan Puutarha- ja Hautauspalvelu
+61 KoiHu Adventures
+62 Pogostan sanomat
+63 Mantsin Moniapu
+
+64 MJAN Palvelut
+65 Malahvin kotileipomo
+
+66 Hoiva ja Huolenpito Kotipiha Oy
+
+67 
+
+68 Loma-asunto Myllymatti
+
+69 
+
+70 Pogostan Kirppis
+71 Ilomantsin Nukke- ja nalletalo 
+72 
+
+73 Ilomantsin Betonituote Kautto Oy
+
+74 
+
+75 
+
+76 
+
+77 Kiinteistöhuolto Martiskainen 
+
+78 Kentsun Autopalvelut 
+79 TL Toimistopalvelut
+80 Ylisoutajan jäätelökioski 
+81 Asianajotoimisto Teräskulma  
+
+82 Teatteri Möhkö 
+
+83 Mantan Luontopalvelut
+
+84 Möhkön Rajakartano  
+
+85 Tähkä-Design 
+
+86 Ilomantsin Kirjasto
+
+87 Möyhäyttäjä  
+
+88 Konepalvelu Sami Hassinen 
+
+89 Ilomantsin Optiikka 
+
+90 Sonkajan kylätalo
+
+91 Kilihakka
+
+92 Sonkajan uimaranta
+
+93 Luontopirtti Mesikkä 
+94 Rajakenraalinmaja 
+
+95 Evankelis-luterilainen kirkko 
+
+96 Pyhän Profeetta Elian kirkko 
+
+97 Rantakahvila Ilonpisara
+
+98 Sonkajan tanssilava 
+
+99 Kivilahden Nuorisoseurantalo / Kylätalo
+
+100 Veneranta 
+101 Kivilahden Uimaranta
+
+102 Rosvolahden kota
+
+103 Lylykoski
+
+104 Petäjäkangas
+
+105 Tokrajärven uimaranta
+
+106 Möhkön Arboretum
+
+107 Taistelijan talo 
+]
